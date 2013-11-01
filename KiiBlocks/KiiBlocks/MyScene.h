63b0b0287a4768f35c7d8d897ca12d1c10b3d6a1
Chris Beauchamp <cjbeauchamp@gmail.com>
2013-10-31 18:57:26 -0700

    updated license and cleanup

--- KiiBlocks/KiiBlocks/MyScene.h
+++ KiiBlocks/KiiBlocks/MyScene.h
@@ -1,9 +1,20 @@
 //
-//  MyScene.h
-//  KiiBlocks
 //
-
-//  Copyright (c) 2013 Kii Corporation. All rights reserved.
+// Copyright 2013 Kii Corporation
+// http://kii.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 //
 
 #import <SpriteKit/SpriteKit.h>
